23d3769a4016f494ff3be341228239988264f974
sec_goat <joshua.tilson@yahoo.com>
2015-03-11 18:10:33 -0400

    Moved UnityTestRepo (Roll A Ball) into this repo as well ast started an new Unity Tutorial for Space Shooter.

---
+++ RollABall/Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    void Update()
+    {
+
+    }
+	public float speed;
+	public TextMesh countText;
+	public TextMesh winText;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 5) 
+		{
+			winText.text = "YOU WIN!!!";
+		}
+	}
+    void FixedUpdate() //called right before physics checks etc.
+    {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+
+    }
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.gameObject.tag == "Pickup")
+		{
+			other.gameObject.SetActive(false);
+			count += 1;
+			setCountText();
+		}
+
+	}
+
+
+
+}
